f17369210d4778ab2f87d8fa752bf939ac04fb39
iamdkshah <iamdkshah27@gmail.com>
2019-11-21 12:56:18 +0545

    static data

--- app/src/main/java/com/example/esoftwaricaassign3/MainActivity.java
+++ app/src/main/java/com/example/esoftwaricaassign3/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.esoftwaricaassign3;
 import android.os.Bundle;
 
 import com.example.esoftwaricaassign3.adapters.UserAdapter;
+import com.example.esoftwaricaassign3.models.UserModel;
 import com.example.esoftwaricaassign3.ui.dashboard.AddUser;
 import com.example.esoftwaricaassign3.ui.home.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -13,6 +14,9 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -22,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+        AddUser.userDetails.add(new UserModel("msms", "45","asda", "male"));
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
@@ -29,4 +35,5 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
+
 }
